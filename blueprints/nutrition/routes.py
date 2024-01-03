cfd5b2d394ab084c8db1e25de8280466b7f7ab24
James Mockford <james.mockford@outlook.com>
2024-01-03 12:38:29 +0000

    meals-and-food routes

--- blueprints/nutrition/routes.py
+++ blueprints/nutrition/routes.py
@@ -2,20 +2,40 @@ from flask import Blueprint, render_template
 
 nutrition_blueprint = Blueprint('nutrition', __name__, url_prefix='/nutrition')
 
-# Define routes within this blueprint
+# Define nutrition routes
+
 @nutrition_blueprint.route('/')
 def nutrition_home():
-    # Define the route logic
     return render_template('nutrition/nutrition-home.html')
 
 @nutrition_blueprint.route('/meals-and-foods')
 def mealsAndFoods():
-    # Define the route logic
     return render_template('nutrition/meals-and-food/meals-and-food-home.html')
+
+# Define food routes
 
 @nutrition_blueprint.route('/meals-and-foods/create-food')
 def createFood():
-    # Define the route logic
     return render_template('nutrition/meals-and-food/food/create-food.html')
 
-# Define other routes for the nutrition category
+@nutrition_blueprint.route('/meals-and-foods/browse-food')
+def browseFood():
+    return render_template('nutrition/meals-and-food/food/browse-food.html')
+
+@nutrition_blueprint.route('/meals-and-foods/add-food')
+def addFood():
+    return render_template('nutrition/meals-and-food/food/add-food.html')
+
+# Define meal routes
+
+@nutrition_blueprint.route('/meals-and-foods/create-meal')
+def createMeal():
+    return render_template('nutrition/meals-and-food/meal/create-meal.html')
+
+@nutrition_blueprint.route('/meals-and-foods/browse-meal')
+def browseMeal():
+    return render_template('nutrition/meals-and-food/meal/browse-meal.html')
+
+@nutrition_blueprint.route('/meals-and-foods/add-meal')
+def addMeal():
+    return render_template('nutrition/meals-and-food/meal/add-meal.html')
