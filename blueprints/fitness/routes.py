b7f9260f70d9d9774dd9c39c6a8e47f72946e3ce
James Mockford <james.mockford@outlook.com>
2024-01-11 15:00:18 +0000

    Fitness Page

--- blueprints/fitness/routes.py
+++ blueprints/fitness/routes.py
@@ -5,7 +5,7 @@ fitness_blueprint = Blueprint('fitness', __name__)
 
 # Define routes within the fitness blueprint
 @fitness_blueprint.route('/fitness')
-def fitness():
-    return render_template('fitness.html')
+def fitness_home():
+    return render_template('fitness/fitness-home.html')
 
 # You can add more routes here for the fitness section if needed
